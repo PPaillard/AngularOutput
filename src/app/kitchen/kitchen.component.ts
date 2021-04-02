637d37be1c06524ff492703b8cc9300f1a61be82
Pierre Paillard <paillard.pierre@gmail.com>
2021-04-02 15:20:25 +0200

    Output integré

---
+++ src/app/kitchen/kitchen.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kitchen',
+  templateUrl: './kitchen.component.html',
+  styleUrls: ['./kitchen.component.css']
+})
+export class KitchenComponent implements OnInit {
+
+  public cook:boolean = false;
+  public flourQuantity:number;
+  public saltQuantity:number;
+  public sugarQuantity:number;
+  public eggQuantity:number;
+  public numberOfCookies:number;
+
+  public cookiesCooked:number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public startCooking() {
+    this.cook = true;
+  }
+
+  onGetCookies($event:number) {
+    this.cookiesCooked = $event;
+  }
+
+}
---
+++ src/app/magic-oven/magic-oven.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-magic-oven',
+  templateUrl: './magic-oven.component.html',
+  styleUrls: ['./magic-oven.component.css']
+})
+export class MagicOvenComponent implements OnInit {
+  @Input() startCooking: boolean
+  @Input() nbCookies: number
+  @Input() flour: number
+  @Input() salt: number
+  @Input() sugar: number
+  @Input() egg: number
+
+  @Output() getCookies: EventEmitter<number> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  sendCookies(){
+    alert(this.nbCookies)
+    this.getCookies.emit(this.nbCookies);
+  }
+
+}
